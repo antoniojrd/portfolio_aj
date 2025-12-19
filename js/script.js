3d2eea539b446a8b4cbb947248989448581cdc6a
antoniojr <antoniojrfsousadev@gmail.com>
2025-12-19 10:34:43 -0300

    ajuste_responsividade

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 const toggle =document.getElementById('menu-toggle');
 const menu = document.querySelector('nav ul');
-
+const menuItens = menu.querySelectorAll('li a');
 toggle.addEventListener('click', () => {
     menu.classList.toggle('active')
 });
+
+menuItens.forEach(item => {
+    item.addEventListener('click', ()=> {
+        menu.classList.remove('active');
+    })
+})
